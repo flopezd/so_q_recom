5426cfd74322d654091384d54b3a6c79b3e52d7d
flopezd <fdestain@fi.uba.ar>
2023-03-30 11:58:03 -0300

    filter documents for doc2vec training

--- notebooks/text_model/doc2vec_trainer.py
+++ notebooks/text_model/doc2vec_trainer.py
@@ -1,35 +1,54 @@
 import psycopg2
 import gensim
-import faiss
-import numpy as np
+import argparse
 
 class DBCorpus():
-    def __init__(self) -> None:
-        connection = psycopg2.connect(user="postgres", password="postgres", host="127.0.0.1", port="63333", database="stack_overflow")
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute("""select id, title, body_text
-                from posts_19
-                where body_text is not null
-                limit 500""")
-                self.posts = cursor.fetchall()
+    def __init__(self, port, tag, until_year, batch_size) -> None:
+        self.connection = psycopg2.connect(user="postgres", password="postgres", host="127.0.0.1", port=port, database="stack_overflow")
+        self.tag, self.until_year, self.batch_size = tag, until_year, batch_size
+
+    def _get_batch(self, idx):
+        with self.connection:
+            with self.connection.cursor() as cursor:
+                cursor.execute(f"""select p.id, p.title, p.body_text
+                from posts_19 p
+                join posts_19_tags pt on pt.post_id=p.id
+                join tags t on pt.tag_id=t.id
+                where p.body_text is not null
+                and creation_date < '{self.until_year}-01-01'
+                and t.tag_name = '{self.tag}'
+                offset {self.batch_size*idx} limit {self.batch_size}""")
+                return cursor.fetchall()
 
     def __iter__(self):
-        for p_id, title, body in self.posts:
-            post_text = body
-            if title is not None:
-                post_text = title + "\n\n" + body
-            tokens = gensim.utils.simple_preprocess(post_text)
-            yield gensim.models.doc2vec.TaggedDocument(tokens, [p_id])
+        idx = 0
+        posts = self._get_batch(idx)
+        while len(posts) > 0:
+            for p_id, title, body in posts:
+                post_text = body
+                if title is not None:
+                    post_text = title + "\n\n" + body
+                tokens = gensim.utils.simple_preprocess(post_text)
+                yield gensim.models.doc2vec.TaggedDocument(tokens, [p_id])
+            
+            idx += 1
+            posts = self._get_batch(idx)
             
 
 class Doc2VecTrainer:
-    def __init__(self) -> None:
-        self.corpus = DBCorpus()
+    def __init__(self, corpus) -> None:
+        self.corpus = corpus
 
     def run(self):
-        model = gensim.models.doc2vec.Doc2Vec(self.corpus, vector_size=50, epochs=10, hs=1, min_count=100, workers=7)
+        model = gensim.models.doc2vec.Doc2Vec(self.corpus, vector_size=50, epochs=20, hs=1, min_count=100, workers=7)
         model.save('model.doc')
 
 if __name__ == "__main__":
-    Doc2VecTrainer().run()
+    parser = argparse.ArgumentParser(description='Train doc2vec.')
+    parser.add_argument('port')
+    parser.add_argument('tag')
+    parser.add_argument('until_year')
+    parser.add_argument('batch_size', type=int)
+    args = parser.parse_args()
+
+    Doc2VecTrainer(DBCorpus(args.port, args.tag, args.until_year, args.batch_size)).run()
